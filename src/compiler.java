6020adcf55c02b865c96ca0403fb9553724992ea
Florian Weiß <dragonu1@web.de>
2019-01-13 00:01:34 +0100

    Hoffentlich sind hier die änderungen von name service und requesthandler drin

--- src/compiler.java
+++ src/compiler.java
@@ -14,7 +14,8 @@ public class compiler {
         String wClass = "";
         String module = "";
         String newLine = "";
-        Path path = null;
+        Path pathImpl = null;
+        Path pathHandler = null;
         byte[] bytes;
         int counter = 0;
         try {
@@ -30,31 +31,49 @@ public class compiler {
                         wClass = parts[1];
                         if (!Files.exists(Paths.get(module)))
                             new File(module).mkdirs();
-                        path = Paths.get(module+fileSeparator+"_"+wClass+"ImplBase.java");
-                        String text = "package "+module+";\n\n" +
+                        pathImpl = Paths.get(module+fileSeparator+"_"+wClass+"ImplBase.java");
+                        String textImpl = "package "+module+";\n\n" +
                                 "public abstract class _"+parts[1]+"ImplBase {\n\n";
-                        bytes = text.getBytes();
-                        Files.write(path,bytes,StandardOpenOption.CREATE_NEW,StandardOpenOption.WRITE);
+                        bytes = textImpl.getBytes();
+                        Files.write(pathImpl,bytes,StandardOpenOption.CREATE_NEW,StandardOpenOption.WRITE);
+                        pathHandler = Paths.get(module+fileSeparator+"_"+wClass+"ImplBase.java");
+                        String textHandler = "package "+module+";\n\n" +
+                                "public  class _"+parts[1]+"Handler {\n\n";
+                        bytes = textHandler.getBytes();
+                        Files.write(pathHandler,bytes,StandardOpenOption.CREATE_NEW,StandardOpenOption.WRITE);
                         break;
                     case "\t};" :
-                        text = "\t\tpublic static _"+wClass+"ImplBase narrowCast(Object rawObjectRef){\n" +
+                        textImpl = "\t\tpublic static _"+wClass+"ImplBase narrowCast(Object rawObjectRef){\n" +
                                 "return new _"+wClass+"Handler(rawObjectRef);\n}";
-                        bytes = text.getBytes();
-                        Files.write(path,bytes,StandardOpenOption.APPEND,StandardOpenOption.WRITE);
+                        bytes = textImpl.getBytes();
+                        Files.write(pathImpl,bytes,StandardOpenOption.APPEND,StandardOpenOption.WRITE);
+                        textHandler = "\t\tpublic static _"+wClass+"Handler narrowCast(Object rawObjectRef){\n" +
+                        "return new _"+wClass+"Handler(\n" +
+                        "private String name = rawObjectRef.toString().split(\",\")[0];\n" +
+                        "private String host = rawObjectRef.toString().split(\",\")[1];\n" +
+                        "private int port = Integer.parseInt(rawObjectRef.toString().split(\",\")[2]);\n";
+                        bytes = textHandler.getBytes();
+                        Files.write(pathHandler,bytes,StandardOpenOption.APPEND,StandardOpenOption.WRITE);
                         break;
                     case "};":
-                        text = "}";
-                        bytes = text.getBytes();
-                        Files.write(path,bytes,StandardOpenOption.APPEND,StandardOpenOption.WRITE);
+                        textImpl = "}";
+                        bytes = textImpl.getBytes();
+                        Files.write(pathImpl,bytes,StandardOpenOption.APPEND,StandardOpenOption.WRITE);
                         break;
                     default:
                         newLine = "";
                         for (String a:parts) {
                                 newLine = newLine +" "+a.replace("\t","");
                         }
-                        text = "\tpublic abstract "+newLine.replace("string","String")+"\n";
-                        bytes = text.getBytes();
-                        Files.write(path,bytes,StandardOpenOption.APPEND,StandardOpenOption.WRITE);
+                        textImpl = "\tpublic abstract "+newLine.replace("string","String")+"\n";
+                        bytes = textImpl.getBytes();
+                        Files.write(pathImpl,bytes,StandardOpenOption.APPEND,StandardOpenOption.WRITE);
+                        textHandler = "\tpublic "+newLine.replace("string","String")+" throws RuntimeException\n"+
+                        "Object result = RemoteDelegator.invokeMethod(name, host, port, \"_BankImplBase\", \"balanceInquiry\");\n"+
+                        "if (result instanceof RuntimeException) throw (RuntimeException) result;\n"+
+                        "return String.valueOf(result);\n";
+                        bytes = textHandler.getBytes();
+                        Files.write(pathHandler,bytes,StandardOpenOption.APPEND,StandardOpenOption.WRITE);
                         break;
                 }
                 line = reader.readLine();
